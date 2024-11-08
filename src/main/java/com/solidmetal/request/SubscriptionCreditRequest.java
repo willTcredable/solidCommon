2885f5ba4f85ef88729d717335d85a23eb907a28
lmahinja <lmahinja@credable.io>
2024-11-08 11:15:28 +0300

    Rename a class

--- src/main/java/com/solidmetal/request/SubscriptionCreditRequest.java
+++ src/main/java/com/solidmetal/request/SubscriptionCreditRequest.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 
 @Data
-public class SubscriptionRequest {
+public class SubscriptionCreditRequest {
 
     private String customerNumber;
     private String productCode;
