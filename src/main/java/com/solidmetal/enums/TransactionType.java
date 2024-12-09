3b976d747af86d40c963f0a154654d7576958cfa
lmahinja <lmahinja@credable.io>
2024-12-09 15:35:35 +0300

    Update get enum from string

--- src/main/java/com/solidmetal/enums/TransactionType.java
+++ src/main/java/com/solidmetal/enums/TransactionType.java
@@ -3,12 +3,26 @@ package com.solidmetal.enums;
 public enum TransactionType {
 
     DISBURSEMENT,
+    REPAYMENT,
     NORMAL_REPAYMENT,
     AUTO_STRIKE_REPAYMENT,
+    AUTO_REPAYMENT,
     PENALTY,
     DEBIT,
-    CREDIT;
+    CREDIT,
+    NONE;
 
     private TransactionType() {
+    }
+
+    public static TransactionType fromString(String name) {
+        try {
+            if (name != null) {
+                return TransactionType.valueOf(name.toUpperCase());
+            }
+        } catch (IllegalArgumentException e) {
+            // Enum not found
+        }
+        return NONE;
     }
 }
